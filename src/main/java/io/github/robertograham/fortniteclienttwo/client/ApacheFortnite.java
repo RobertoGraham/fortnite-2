3c1e9fa757efc397beee2b40036bf57530f930a4
unknown <robertogrhm@gmail.com>
2018-12-17 14:09:30 +0000

    Added barebones Fortnite interface, barebones Apache HTTP Client impl of Fortnite and builders for each

---
+++ src/main/java/io/github/robertograham/fortniteclienttwo/client/ApacheFortnite.java
@@ -0,0 +1,106 @@
+package io.github.robertograham.fortniteclienttwo.client;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.util.Objects;
+
+public final class ApacheFortnite implements Fortnite {
+
+    private final String epicGamesEmailAddress;
+    private final String epicGamesPassword;
+    private final String epicGamesLauncherToken;
+    private final String fortniteClientToken;
+    private final HttpClient httpClient;
+
+    private ApacheFortnite(Builder builder) {
+        epicGamesEmailAddress = builder.epicGamesEmailAddress();
+        epicGamesPassword = builder.epicGamesPassword();
+        epicGamesLauncherToken = builder.epicGamesLauncherToken();
+        fortniteClientToken = builder.fortniteClientToken();
+        httpClient = builder.httpClient;
+    }
+
+    private static ApacheFortnite newInstance(Builder builder) {
+        return new ApacheFortnite(builder);
+    }
+
+    @Override
+    public String toString() {
+        return "ApacheFortnite{" +
+                "epicGamesEmailAddress='" + epicGamesEmailAddress + '\'' +
+                ", epicGamesPassword='" + epicGamesPassword + '\'' +
+                ", epicGamesLauncherToken='" + epicGamesLauncherToken + '\'' +
+                ", fortniteClientToken='" + fortniteClientToken + '\'' +
+                ", httpClient=" + httpClient +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object)
+            return true;
+        if (!(object instanceof ApacheFortnite))
+            return false;
+        ApacheFortnite apacheFortnite = (ApacheFortnite) object;
+        return epicGamesEmailAddress.equals(apacheFortnite.epicGamesEmailAddress) &&
+                epicGamesPassword.equals(apacheFortnite.epicGamesPassword) &&
+                epicGamesLauncherToken.equals(apacheFortnite.epicGamesLauncherToken) &&
+                fortniteClientToken.equals(apacheFortnite.fortniteClientToken) &&
+                httpClient.equals(apacheFortnite.httpClient);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(epicGamesEmailAddress, epicGamesPassword, epicGamesLauncherToken, fortniteClientToken, httpClient);
+    }
+
+    public static final class Builder extends Fortnite.Builder<Builder> {
+
+        private HttpClient httpClient = HttpClientBuilder.create()
+                .build();
+
+        public Builder(String epicGamesEmailAddress, String epicGamesPassword) {
+            super(epicGamesEmailAddress, epicGamesPassword);
+        }
+
+        public Builder addHttpClient(HttpClient httpClient) {
+            this.httpClient = Objects.requireNonNull(httpClient, "httpClient cannot be null");
+            return this;
+        }
+
+        @Override
+        public Fortnite build() {
+            return newInstance(this);
+        }
+
+        @Override
+        Builder self() {
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "Builder{" +
+                    "httpClient=" + httpClient +
+                    "} " + super.toString();
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (this == object)
+                return true;
+            if (!(object instanceof Builder))
+                return false;
+            if (!super.equals(object))
+                return false;
+            Builder builder = (Builder) object;
+            return httpClient.equals(builder.httpClient);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(super.hashCode(), httpClient);
+        }
+    }
+}
---
+++ src/main/java/io/github/robertograham/fortniteclienttwo/client/Fortnite.java
@@ -0,0 +1,77 @@
+package io.github.robertograham.fortniteclienttwo.client;
+
+import java.util.Objects;
+
+public interface Fortnite {
+
+    abstract class Builder<T extends Builder<T>> {
+
+        private final String epicGamesEmailAddress;
+        private final String epicGamesPassword;
+        private String epicGamesLauncherToken = "default";
+        private String fortniteClientToken = "default";
+
+        Builder(String epicGamesEmailAddress, String epicGamesPassword) {
+            this.epicGamesEmailAddress = Objects.requireNonNull(epicGamesEmailAddress, "epicGamesEmailAddress cannot be null");
+            this.epicGamesPassword = Objects.requireNonNull(epicGamesPassword, "epicGamesPassword cannot be null");
+        }
+
+        public final T addEpicGamesLauncherToken(String epicGamesLauncherToken) {
+            this.epicGamesLauncherToken = Objects.requireNonNull(epicGamesLauncherToken, "epicGamesLauncherToken cannot be null");
+            return self();
+        }
+
+        public final T addFortniteClientToken(String fortniteClientToken) {
+            this.fortniteClientToken = Objects.requireNonNull(fortniteClientToken, "fortniteClientToken cannot be null");
+            return self();
+        }
+
+        public abstract Fortnite build();
+
+        abstract T self();
+
+        final String epicGamesEmailAddress() {
+            return epicGamesEmailAddress;
+        }
+
+        final String epicGamesPassword() {
+            return epicGamesPassword;
+        }
+
+        final String epicGamesLauncherToken() {
+            return epicGamesLauncherToken;
+        }
+
+        final String fortniteClientToken() {
+            return fortniteClientToken;
+        }
+
+        @Override
+        public String toString() {
+            return "Builder{" +
+                    "epicGamesEmailAddress='" + epicGamesEmailAddress + '\'' +
+                    ", epicGamesPassword='" + epicGamesPassword + '\'' +
+                    ", epicGamesLauncherToken='" + epicGamesLauncherToken + '\'' +
+                    ", fortniteClientToken='" + fortniteClientToken + '\'' +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (this == object)
+                return true;
+            if (!(object instanceof Builder))
+                return false;
+            Builder<?> builder = (Builder<?>) object;
+            return epicGamesEmailAddress.equals(builder.epicGamesEmailAddress) &&
+                    epicGamesPassword.equals(builder.epicGamesPassword) &&
+                    epicGamesLauncherToken.equals(builder.epicGamesLauncherToken) &&
+                    fortniteClientToken.equals(builder.fortniteClientToken);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(epicGamesEmailAddress, epicGamesPassword, epicGamesLauncherToken, fortniteClientToken);
+        }
+    }
+}
